bd1de8c34108ac0911495eeab31c755646b9ca39
nohabianombres <josempavas@gmail.com>
2023-06-10 18:35:20 -0500

    generar_facura_servicio_productos

--- front/recepcionista/ventanasRecepcionista.py
+++ front/recepcionista/ventanasRecepcionista.py
@@ -1,14 +1,11 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.uic import loadUi
 
 import sys
 
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication
-from Front.comunes.emerComunes import *
-
-
-
+from Front.comunes.emerComunes import emerRetorno, emerBuscUsu, emerAgrCli, emerBuscSer, emerAgrCita, emerCanCitaId, emerBusCitas,  emerBuscClien, emerCanCita, emerAdiFal
+from Back.Usuarios import Recepcion
 
 class Recepcionista(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -31,19 +28,26 @@ class Recepcionista(QtWidgets.QMainWindow):
         if sender_button_rec == self.botSerRec:
             self.hide()
             self.recServicios = RecepcionistaServicios()
+            self.recServicios.recibir_datos(self.datos_usuario)
             self.recServicios.show()
         elif sender_button_rec == self.botUsuRec:
             self.hide()
             self.recUsuarios = RecepcionistaUsuarios()
+            self.recUsuarios.recibir_datos(self.datos_usuario)
             self.recUsuarios.show()
         elif sender_button_rec == self.botAgeRec:
             self.hide()
             self.recAgenda = RecepcionistaAgenda()
+            self.recAgenda.recibir_datos(self.datos_usuario)
             self.recAgenda.show()
         elif sender_button_rec == self.botCliRec:
             self.hide()
             self.recClientes = RecepcionistaClientes()
+            self.recClientes.recibir_datos(self.datos_usuario)
             self.recClientes.show()
+
+    def recibir_datos (self, usuario_validar):
+        self.datos_usuario = usuario_validar
 
 class RecepcionistaClientes(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -54,10 +58,11 @@ class RecepcionistaClientes(QtWidgets.QMainWindow):
         self.botCliRec.clicked.connect(self.open_view_rec_cli)
         self.botSerRec.clicked.connect(self.open_view_rec_cli)
         self.botUsuRec.clicked.connect(self.open_view_rec_cli)
-        self.botAdiFalRec.clicked.connect(self.open_view_eme_AdiFal)
+
+        self.botAdiFalRec.clicked.connect(self.open_view_eme_AdcFal)
         self.botAgrCliRec.clicked.connect(self.open_view_eme_AgrCli)
         self.botBusCliRec.clicked.connect(self.open_view_eme_BusCli)
-        self.botModCliRec.clicked.connect(self.open_view_eme_ModCli)
+
 
         self.recServicios = None
         self.recAgenda = None
@@ -69,34 +74,49 @@ class RecepcionistaClientes(QtWidgets.QMainWindow):
         if sender_button_rec_cli == self.botSerRec:
             self.hide()
             self.recServicios = RecepcionistaServicios()
+            self.recServicios.recibir_datos(self.datos_usuario)
             self.recServicios.show()
         elif sender_button_rec_cli == self.botUsuRec:
             self.hide()
             self.recUsuarios = RecepcionistaUsuarios()
+            self.recUsuarios.recibir_datos(self.datos_usuario)
             self.recUsuarios.show()
         elif sender_button_rec_cli == self.botAgeRec:
             self.hide()
             self.recAgenda = RecepcionistaAgenda()
+            self.recAgenda.recibir_datos(self.datos_usuario)
             self.recAgenda.show()
         elif sender_button_rec_cli == self.botCliRec:
             self.hide()
             self.recClientes = RecepcionistaClientes()
+            self.recClientes.recibir_datos(self.datos_usuario)
             self.recClientes.show()
 
-    def open_view_eme_AdiFal(self):
-        self.AdiFal = emerAdiFal()
-        self.AdiFal.show()
+    def open_view_eme_AdcFal(self):
+        self.AdcFal = emerAdiFal()
+        self.AdcFal.set_callback(self.crear_ventana)
+        self.AdcFal.show()
 
     def open_view_eme_AgrCli(self):
         self.AgrCli = emerAgrCli()
+        self.AgrCli.set_callback(self.crear_ventana)
         self.AgrCli.show()
 
     def open_view_eme_BusCli(self):
+        self.BusCli = emerBuscClien()
+        self.BusCli.set_callback(self.imprimir_tablas)
+        self.BusCli.show()
+
+    def crear_ventana(self, retorno):
+        self.emer_retorno = emerRetorno()
+        self.emer_retorno.imprimir_retorno(retorno)
+        self.emer_retorno.show()
+
+    def imprimir_tablas(self):
         pass
 
-    def open_view_eme_ModCli(self):
-        self.ModCli = emerModClien()
-        self.ModCli.show()
+    def recibir_datos(self, usuario_validar):
+        self.datos_usuario = usuario_validar
 
 class RecepcionistaAgenda(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -107,10 +127,11 @@ class RecepcionistaAgenda(QtWidgets.QMainWindow):
         self.botCliRec.clicked.connect(self.open_view_rec_Age)
         self.botSerRec.clicked.connect(self.open_view_rec_Age)
         self.botUsuRec.clicked.connect(self.open_view_rec_Age)
-        self.botBusCitRec.clicked.connect(self.open_view_eme_BusCit)
-        self.botCanCitRec.clicked.connect(self.open_view_eme_CanCit)
-        self.botCreCitRec.clicked.connect(self.open_view_eme_CreCit)
-        self.botModCitRec.clicked.connect(self.open_view_eme_ModCit)
+
+        self.botBusCitRec.clicked.connect(self.open_view_eme_BusCita)
+        self.botCanCitRec.clicked.connect(self.open_view_eme_CanCita)
+        self.botCreCitRec.clicked.connect(self.open_view_eme_AgrCit)
+
 
 
         self.recServicios = None
@@ -124,34 +145,68 @@ class RecepcionistaAgenda(QtWidgets.QMainWindow):
         if sender_button_rec_age == self.botSerRec:
             self.hide()
             self.recServicios = RecepcionistaServicios()
+            self.recServicios.recibir_datos(self.datos_usuario)
             self.recServicios.show()
         elif sender_button_rec_age == self.botUsuRec:
             self.hide()
             self.recUsuarios = RecepcionistaUsuarios()
+            self.recUsuarios.recibir_datos(self.datos_usuario)
             self.recUsuarios.show()
         elif sender_button_rec_age == self.botAgeRec:
             self.hide()
             self.recAgenda = RecepcionistaAgenda()
+            self.recAgenda.recibir_datos(self.datos_usuario)
             self.recAgenda.show()
         elif sender_button_rec_age == self.botCliRec:
             self.hide()
             self.recClientes = RecepcionistaClientes()
+            self.recClientes.recibir_datos(self.datos_usuario)
             self.recClientes.show()
 
-    def open_view_eme_BusCit(self):
-        self.BusCit = emerBusCita()
+    def open_view_eme_AgrCit(self):
+        self.AgrCit = emerAgrCita()
+        self.AgrCit.set_callback(self.crear_ventana)
+        self.AgrCit.show()
+
+    def open_view_eme_BusCita(self):
+        self.BusCit = emerBusCitas()
+        self.BusCit.set_callback(self.imprimir_tablas)
         self.BusCit.show()
 
-    def open_view_eme_CanCit(self):
-        self.CanCit = emerCamCita()
-        self.CanCit.show()
-
-    def open_view_eme_ModCit(self):
+    def open_view_eme_ModCita(self):
         pass
 
-    def open_view_eme_CreCit(self):
-        self.CreCit = emerAgrCita()
-        self.CreCit.show()
+    def open_view_eme_CanCita(self):
+        self.CanCita = emerCanCita()
+        self.CanCita.set_callback(self.open_view_emerIdCitCan)
+        self.CanCita.show()
+
+    def imprimir_tablas(self, listas):
+        print('entre:')
+        # self.ui.TabAgenda.clearContents()  # Limpiar contenido existente en la tabla
+        # self.ui.TabAgenda.setRowCount()  # Reiniciar número de filas
+
+        for lista in listas:
+            print(lista)
+            fila = self.ui.TabAgenda.rowCount()
+            self.ui.TabAgenda.insertRow(fila)
+
+            for columna, elemento in enumerate(lista):
+                celda = QtWidgets.QTableWidgetItem(elemento)
+                self.ui.TabAgenda.setItem(fila, columna, celda)
+
+    def crear_ventana(self, retorno):
+        self.emer_retorno = emerRetorno()
+        self.emer_retorno.imprimir_retorno(retorno)
+        self.emer_retorno.show()
+
+    def open_view_emerIdCitCan(self, a):
+        self.IdCitCan = emerCanCitaId()
+        self.IdCitCan.set_callback(self.crear_ventana)
+        self.IdCitCan.show()
+
+    def recibir_datos(self, usuario_validar):
+        self.datos_usuario = usuario_validar
 
 class RecepcionistaServicios(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -162,41 +217,57 @@ class RecepcionistaServicios(QtWidgets.QMainWindow):
         self.botCliRec.clicked.connect(self.open_view_rec_Ser)
         self.botSerRec.clicked.connect(self.open_view_rec_Ser)
         self.botUsuRec.clicked.connect(self.open_view_rec_Ser)
+
         self.botBusSerRec.clicked.connect(self.open_view_eme_BusSer)
-        self.botMosTodRec.clicked.connect(self.open_view_eme_MosTod)
+        self.botMosTodRec.clicked.connect(self.open_view_eme_MosTodSer)
 
         self.recServicios = None
         self.recAgenda = None
         self.recUsuarios = None
         self.recClientes = None
 
-    def open_view_eme_BusSer(self):
-        self.BusSer = emerBuscSer()
-        self.BusSer.show()
-
-    def open_view_eme_MosTod(self):
-        pass
-
     def open_view_rec_Ser(self):
-        sender_button_rec_ser = self.sender()
-        if sender_button_rec_ser == self.botSerRec:
+
+        sender_button_rec_age = self.sender()
+        if sender_button_rec_age == self.botSerRec:
             self.hide()
             self.recServicios = RecepcionistaServicios()
+            self.recServicios.recibir_datos(self.datos_usuario)
             self.recServicios.show()
-        elif sender_button_rec_ser == self.botUsuRec:
+        elif sender_button_rec_age == self.botUsuRec:
             self.hide()
             self.recUsuarios = RecepcionistaUsuarios()
+            self.recUsuarios.recibir_datos(self.datos_usuario)
             self.recUsuarios.show()
-        elif sender_button_rec_ser == self.botAgeRec:
+        elif sender_button_rec_age == self.botAgeRec:
             self.hide()
             self.recAgenda = RecepcionistaAgenda()
+            self.recAgenda.recibir_datos(self.datos_usuario)
             self.recAgenda.show()
-        elif sender_button_rec_ser == self.botCliRec:
+        elif sender_button_rec_age == self.botCliRec:
             self.hide()
             self.recClientes = RecepcionistaClientes()
+            self.recClientes.recibir_datos(self.datos_usuario)
             self.recClientes.show()
 
+    def open_view_eme_BusSer(self):
+        self.BusSer = emerBuscSer()
+        self.BusSer.set_callback(self.imprimir_tabla)
+        self.BusSer.show()
 
+    def open_view_eme_MosTodSer(self):
+        pass
+
+    def crear_ventana(self, retorno):
+        self.emer_retorno = emerRetorno()
+        self.emer_retorno.imprimir_retorno(retorno)
+        self.emer_retorno.show()
+
+    def imprimir_tabla(self):
+        pass
+
+    def recibir_datos (self, usuario_validar):
+        self.datos_usuario = usuario_validar
 
 class RecepcionistaUsuarios(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -207,6 +278,7 @@ class RecepcionistaUsuarios(QtWidgets.QMainWindow):
         self.botCliRec.clicked.connect(self.open_view_rec_usu)
         self.botSerRec.clicked.connect(self.open_view_rec_usu)
         self.botUsuRec.clicked.connect(self.open_view_rec_usu)
+        self.botBusUsu.clicked.connect(self.open_view_emer_bus_usu)
 
         self.recServicios = None
         self.recAgenda = None
@@ -214,21 +286,44 @@ class RecepcionistaUsuarios(QtWidgets.QMainWindow):
         self.recClientes = None
 
     def open_view_rec_usu(self):
-
         sender_button_rec_usu = self.sender()
         if sender_button_rec_usu == self.botSerRec:
             self.hide()
             self.recServicios = RecepcionistaServicios()
+            self.recServicios.recibir_datos(self.datos_usuario)
             self.recServicios.show()
         elif sender_button_rec_usu == self.botUsuRec:
             self.hide()
             self.recUsuarios = RecepcionistaUsuarios()
+            self.recUsuarios.recibir_datos(self.datos_usuario)
             self.recUsuarios.show()
         elif sender_button_rec_usu == self.botAgeRec:
             self.hide()
             self.recAgenda = RecepcionistaAgenda()
+            self.recAgenda.recibir_datos(self.datos_usuario)
             self.recAgenda.show()
         elif sender_button_rec_usu == self.botCliRec:
             self.hide()
             self.recClientes = RecepcionistaClientes()
+            self.recClientes.recibir_datos(self.datos_usuario)
             self.recClientes.show()
+
+
+    def open_view_emer_bus_usu(self):
+        self.busUsu = emerBuscUsu()
+        self.busUsu.set_callback(self.imprimir_tablas)
+        self.busUsu.recibir_datos(self.datos_usuario)
+        self.busUsu.show()
+
+    def crear_ventana(self, retorno):
+        self.emer_retorno = emerRetorno()
+        self.emer_retorno.imprimir_retorno(retorno)
+        self.emer_retorno.show()
+
+    def imprimir_tablas(self):
+        pass
+
+    def recibir_datos(self, usuario_validar):
+        self.datos_usuario = usuario_validar
+        print(self.datos_usuario)
+        self.ins_recepcionista = Recepcion(usuario_validar[0], usuario_validar[1], usuario_validar[2], usuario_validar[3], usuario_validar[4], usuario_validar[5], usuario_validar[6], usuario_validar[7])
